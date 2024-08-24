2b04bdca6845638243b7cc64975cd6e6922e00c6
Awnish prasad sharma <awnishprasad99@gmail.com>
2024-08-24 21:40:32 +0530

    favicon and hosting setup

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,9 +1,29 @@
 import Header from "./Header";
+import { useState,useRef } from "react";
 
-import { useState } from "react";
+import { checkValidData } from "../Utils/Validate";
+
 
 const LoginPage = () => {
   const [signIn, setSignIn] = useState(true);
+
+  
+const [errorMessage,setErrorMessage] = useState(null);
+
+    const userName = useRef(null)
+    const Email = useRef(null)
+    const Password = useRef(null)
+   
+
+  const handleButtonClick=(e)=>{
+
+console.log("Email",Email.current.value)
+console.log("Password",Password.current.value)
+console.log("UserName", userName.current.value)
+   const Message =  checkValidData(Email.current.value,Password.current.value , userName.current.value) 
+  setErrorMessage(Message)
+  
+  }
 
   const toggleSignIn = () => {
     setSignIn(!signIn);
@@ -20,32 +40,38 @@ const LoginPage = () => {
     >
       <Header />
 
-      <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-14 w-96 bg-black bg-opacity-75">
+      <form onSubmit={(e)=>{e.preventDefault()}}  className="absolute  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-14 py-4   w-1/3 bg-black bg-opacity-75">
         <h1 className="text-white text-3xl pb-4">
           {signIn ? "SignIN" : "SignUp"}
         </h1>
         {!signIn && (
           <input
+          ref={userName}
             type="text"
             placeholder="Enter Your Name"
-            className="p-2 m-2 w-full text-black bg-slate-600 bg-opacity-30"
+            className="p-2 m-2 w-full text-white bg-slate-600 bg-opacity-30"
           />
         )}
 
         <input
           type="text"
+          required
           placeholder="Email Address"
           className="p-2 m-2 w-full text-white   bg-slate-600 bg-opacity-30"
+       ref={Email}
         />
 
         <input
+        ref={Password}
+        required 
           type="password"
           placeholder="Password"
           className="p-2 m-2 w-full  bg-slate-600 bg-opacity-30 text-white"
         />
-        <button className=" font-semibold text-md p-2 m-5 mx-2 block rounded-sm bg-red-600 hover:bg-red-700 text-white w-full text-center">
+        <button onClick={handleButtonClick} className=" font-semibold text-md p-2 m-5 mx-2 block rounded-sm bg-red-600 hover:bg-red-700 text-white w-full text-center">
           {signIn ? "SignIn" : "SignUp "}
         </button>
+        <p className="text-red-500">{errorMessage}</p>
         <p className="text-slate-300 mx-32">OR</p>
         <button className=" font-semibold text-md p-2 m-5 mx-2 block rounded-sm  bg-slate-600 bg-opacity-30 hover:bg-opacity-45 text-white w-full text-center">
           Use a sign-In code
@@ -74,7 +100,6 @@ const LoginPage = () => {
             </p>
           </div>
         </div>
-
         <div className="flex">
           <p className="gap-3 flex-col text-slate-300 opacity-60  text-xs">
             This page is protected by Google reCAPTCHA to ensure you're not a
---
+++ src/Utils/FireBase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDY9qusIluMWtDyGjeAPjnmC9o-xZpSXB4",
+  authDomain: "netflixgpt-32ebc.firebaseapp.com",
+  projectId: "netflixgpt-32ebc",
+  storageBucket: "netflixgpt-32ebc.appspot.com",
+  messagingSenderId: "561533892034",
+  appId: "1:561533892034:web:0becd29d65c2aaae2f925b",
+  measurementId: "G-EHTWZJJD6T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
