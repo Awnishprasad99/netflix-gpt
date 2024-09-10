173d666f082893fafd75ccab963409da0826f571
Awnish prasad sharma <awnishprasad99@gmail.com>
2024-09-10 19:45:15 +0530

    speed commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Body from "./Components/Body";
 import { Provider } from "react-redux";
 import appStore from "./Utils/appStore";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 
@@ -10,6 +11,8 @@ function App() {
     <div className="container">
       <Provider store={appStore}>
         <Body />
+        <SpeedInsights />
+
       
       </Provider>
 
